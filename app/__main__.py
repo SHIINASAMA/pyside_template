9e4f1d8ed5d7512226171ea6aeb20d8f0d84864a
kaoru <shiinasama2001@gmail.com>
2025-03-29 14:31:49 +0800

    Add button to main window and enhance build script with argument parsing

--- app/__main__.py
+++ app/__main__.py
@@ -1,11 +1,19 @@
 import sys
-from PySide6.QtWidgets import QApplication, QMainWindow
+from PySide6.QtWidgets import QApplication, QMainWindow, QMessageBox
 from app.ui_resources.main_window_ui import Ui_MainWindow
+
+ui = Ui_MainWindow()
+
+def click_push_button():
+    QMessageBox.information(None, "Button Clicked", "You clicked the button!")
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     MainWindow = QMainWindow()
-    ui = Ui_MainWindow()
     ui.setupUi(MainWindow)
+
+    # Connect the button click signal to the slot
+    ui.pushButton.clicked.connect(click_push_button)
+
     MainWindow.show()
     sys.exit(app.exec())
--- build.py
+++ build.py
@@ -1,6 +1,31 @@
+import argparse
 import os
 
-if __name__ == '__main__':
+# parse command line arguments
+# --help: show this help message
+parser = argparse.ArgumentParser(description='Build the app.')
+# --ui: only convert ui files to python files
+# --build: only build the app
+# --all: convert ui files and build the app
+mode_group = parser.add_mutually_exclusive_group(required=True)
+mode_group.add_argument('--ui', action='store_true', help='Convert ui files to python files')
+mode_group.add_argument('--build', action='store_true', help='Build the app')
+mode_group.add_argument('--all', action='store_true', help='Convert ui files and build the app')
+# --pyinstaller: use pyinstaller to build the app
+# --nuitka: use nuitka to build the app
+builder_group = parser.add_mutually_exclusive_group(required=True)
+builder_group.add_argument('--pyinstaller', action='store_true', help='Use pyinstaller to build the app')
+builder_group.add_argument('--nuitka', action='store_true', help='Use nuitka to build the app')
+# --onefile: create a single executable file
+# --onedir: create a directory with the executable and all dependencies
+package_format_group = parser.add_mutually_exclusive_group(required=True)
+package_format_group.add_argument('--onefile', action='store_true', help='Create a single executable file')
+package_format_group.add_argument('--onedir', action='store_true',
+                                  help='Create a directory with the executable and all dependencies')
+# do parse
+args = parser.parse_args()
+
+if args.ui or args.all:
     # foreach file in app/ui, and convert it to a .py file using pyside6-uic to app/ui_resources
     for root, dirs, files in os.walk('app/ui'):
         for file in files:
@@ -10,8 +35,29 @@ if __name__ == '__main__':
                 os.system(f'pyside6-uic {input_file} -o {output_file}')
                 print(f'Converted {input_file} to {output_file}')
 
-    # call pyinstaller to build the app
-    # include all files in app package and exclude the ui files
-    # --onefile: create a single executable file
-    # --onedir: create a directory with the executable and all dependencies
-    os.system('pyinstaller --onefile --windowed --add-data "app:app" app/__main__.py --name App')
+if args.build or args.all:
+    if args.pyinstaller:
+        # call pyinstaller to build the app
+        # include all files in app package and exclude the ui files
+        os.system('pyinstaller '
+                  '--quiet '
+                  '--windowed '
+                  '--distpath "build" '
+                  '--workpath "build/work" '
+                  '--add-data "app:app" '
+                  'app/__main__.py '
+                  '--name App'
+                  '--onefile ' if args.onefile else '--onedir ')
+    elif args.nuitka:
+        # call nuitka to build the app
+        # include all files in app package and exclude the ui files
+        os.system('nuitka '
+                  '--quiet '
+                  '--standalone '
+                  '--windows-console-mode=disable '
+                  '--plugin-enable=pyside6 '
+                  '--output-dir=build-nuitka '
+                  '--follow-imports '
+                  '--output-filename="App" '
+                  'app/__main__.py '
+                  '--onefile ' if args.onefile else '--onedir ')
