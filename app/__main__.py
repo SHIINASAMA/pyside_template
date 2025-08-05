d1f673c5c92bba6c1d608996ba328a4941401b49
kaoru <shiinasama2001@gmail.com>
2025-08-06 01:10:19 +0800

    Switch to qasync

--- app/__main__.py
+++ app/__main__.py
@@ -1,6 +1,6 @@
+import asyncio
 import sys
-from PySide6.QtAsyncio import run
-from PySide6.QtWidgets import QApplication
+from qasync import QApplication, run
 from app.main_window import MainWindow
 
 # Importing qdarktheme for dark mode support if needed
@@ -8,8 +8,11 @@ import qdarktheme
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
+    app_close_event = asyncio.Event()
+    app.aboutToQuit.connect(app_close_event.set)
     # Set the themes for the application via qdarktheme
     qdarktheme.setup_theme("auto")
     main_window = MainWindow()
     main_window.show()
-    run()
+    run(app_close_event.wait())
+
